45c4204b69a300cd996ea5279e2b668b2c54a74a
worstvi <157382005+worstvi@users.noreply.github.com>
2024-01-26 19:48:15 +0800

    script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-// script2.js
+// gallery.js
 
 // Пример данных об активностях
 const activities = [
@@ -34,9 +34,31 @@ function showGallery() {
 }
 
 // Функция для уведомления в телеграме
-function notifyTelegram(action) {
-  alert(`Уведомление: ${action}`);
-  // Ваш код для отправки уведомления в телеграме
+async function notifyTelegram(action) {
+  const botToken = 6977179265:AAGkVmDJAWTdxfo6CpS-l4meLTBA4YVZAPg;
+  const chatId = -4105241844;
+  const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: `Уведомление: ${action}`,
+      }),
+    });
+
+    if (response.ok) {
+      alert('Уведомление отправлено в телеграм!');
+    } else {
+      alert('Ошибка отправки уведомления в телеграм.');
+    }
+  } catch (error) {
+    alert('Произошла ошибка: ' + error.message);
+  }
 }
 
 // Вызов функции для отображения галереи после загрузки страницы
